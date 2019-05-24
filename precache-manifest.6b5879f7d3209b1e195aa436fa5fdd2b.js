5fe0021a7c1031f2a5a468c2f92d8f0dd93b2cf1
sweeneyvista <sweeney@wizy.io>
2019-05-24 12:28:15 +0800

    Updates

---
+++ precache-manifest.6b5879f7d3209b1e195aa436fa5fdd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2b5d75ccdd91252497c6b108cf5ee3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f2ff98fea3aae31dd536",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "63c931a45b16531ef618",
+    "url": "/contactmanager/static/js/2.760bc175.chunk.js"
+  },
+  {
+    "revision": "f2ff98fea3aae31dd536",
+    "url": "/contactmanager/static/js/main.39d971e9.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
