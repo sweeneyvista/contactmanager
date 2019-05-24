01752c89a306f74d6ce120beb0885abb7748d138
sweeneyvista <sweeney@wizy.io>
2019-05-24 12:17:14 +0800

    first commit

---
+++ src/components/test/AddContact_ref.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class AddContact extends Component {
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  static defaultProps = {
+    name: "Fred Smith",
+    email: "fred@gmail.com",
+    phone: "777-777-7777"
+  };
+
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control form-control-lg"
+                placeholder="Enter Name..."
+                value={name}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control form-control-lg"
+                placeholder="Enter Email..."
+                value={email}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                className="form-control form-control-lg"
+                placeholder="Enter Phone..."
+                value={phone}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
